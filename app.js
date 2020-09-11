47f608dce7f058f60e888dccf3a554159e8f6eb7
Cristian Labra Castro <clabca@gmail.com>
2020-09-11 13:18:29 -0300

    Segundo Commit

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ $(function() {
         //tome el valor de search y guardelo en variable search
         let search = $('#search').val();
         //muestre por consola la variable search
-        console.log(search);
+        //console.log(search);
+
+        // uso de ajax con jquery
+        $.ajax({
+            url: 'task-search.php',
+            type: 'POST',
+            data: {search},
+            success : function(response){
+                console.log(response);
+            }
+        })
     })
-
-
 });
